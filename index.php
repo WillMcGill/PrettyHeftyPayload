27d27fabeb09bbc17b38bae5c392fb2a552aa3ec
Will McGill <mcgill.will@yahoo.com>
2019-10-30 11:14:16 -0400

    connected to mysql and populating data

--- index.php
+++ index.php
@@ -3,16 +3,32 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$dbname = "PrettyHeftyPayload";
 
-if ($conn = new mysqli($servername, $username, $password)){
-    echo "connected";
-};
+
 
 $myJSON = file_get_contents('./MOCK_DATA.json');
 
 $myData = json_decode($myJSON);
 
-//var_dump($myData);
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+foreach($myData as $value){
+    //echo ($value->first_name . "\n");
+    $fName = $value->first_name;
+    $sql = "INSERT INTO Users (first_name, last_name, gender, ip_address, email) 
+    VALUES ('$value->first_name', '$value->last_name', '$value->gender', '$value->ip_address', '$value->email')";
+    mysqli_query($connection, $sql);
+}
+
+
+
+
+
+
+
+
+var_dump($myData);
 
 // for ($i = 0; $i < count($myData); $i++){
 //     echo ($myData[$i]->first_name . "<br>");
