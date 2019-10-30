9bfb241b337666057c05e4485c26bac09952051a
Will McGill <mcgill.will@yahoo.com>
2019-10-30 12:04:20 -0400

    added check if data exists

--- index.php
+++ index.php
@@ -13,22 +13,25 @@ $myData = json_decode($myJSON);
 
 $connection = mysqli_connect($servername, $username, $password, $dbname);
 
-foreach($myData as $value){
-    //echo ($value->first_name . "\n");
-    $fName = $value->first_name;
-    $sql = "INSERT INTO Users (first_name, last_name, gender, ip_address, email) 
+$dataExists = mysqli_query($connection, "SELECT FIRST_NAME FROM USERS");
+
+if ($dataExists->num_rows === 0) {
+
+    foreach ($myData as $value) {
+        //echo ($value->first_name . "\n");
+        $sql = "INSERT INTO Users (first_name, last_name, gender, ip_address, email) 
     VALUES ('$value->first_name', '$value->last_name', '$value->gender', '$value->ip_address', '$value->email')";
-    mysqli_query($connection, $sql);
+        mysqli_query($connection, $sql);
+    }
 }
 
+echo ($dataExists->num_rows);
 
 
 
 
 
-
-
-var_dump($myData);
+//var_dump($myData);
 
 // for ($i = 0; $i < count($myData); $i++){
 //     echo ($myData[$i]->first_name . "<br>");
